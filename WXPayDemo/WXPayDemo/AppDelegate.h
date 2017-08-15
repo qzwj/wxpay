f445531f90528a0eed35028ccbe99edcea64c6f4
liwenjie <502391211@qq.com>
2017-08-15 09:02:19 +0800

    first commit

---
+++ WXPayDemo/WXPayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WXPayDemo
+//
+//  Created by wjl on 2017/8/14.
+//  Copyright © 2017年 wjl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
